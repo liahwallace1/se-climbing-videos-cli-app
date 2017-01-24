c0c2692835149c29e19124ffb896b4998cf9bcba
Liah Wallace <liahwallace1@gmail.com>
2017-01-24 16:25:38 +0000

    set up scraper methods

--- lib/se_climbing_videos/scraper.rb
+++ lib/se_climbing_videos/scraper.rb
@@ -12,20 +12,35 @@ class SeClimbingVideos::Scraper
 #  end
 
   def self.get_videos_youtube
-    doc = Nokogiri::HTML(open(@webpage))
+    doc = Nokogiri::HTML(open(@youtube_url))
+
+    doc.css("li.div.yt-lockup-video").each do |item|
+      video = SeClimbingVideos::Video.new
+      video.name = doc.search("").text
+      video.upload_user = doc.search("").text
+      video.upload_date = doc.search("").text
+    end
   end
 
   def self.get_videos_vimeo
-    self.get_page.css()
+    sdoc = Nokogiri::HTML(open(@vimeo_url))
+
+    doc.css("").each do |item|
+      video = SeClimbingVideos::Video.new
+      video.name = doc.search("").text
+      video.upload_user = doc.search("").text
+      video.upload_date = doc.search("").text
+    end
   end
 
-  def scrape_videos
-    videos = []
-
-    videos << self.get_videos_youtube
-    videos << self.get_videos_vimeo
-
-    videos
+#  def scrape_videos
+#    videos = []
+#
+#    videos << self.get_videos_youtube
+#    videos << self.get_videos_vimeo
+#
+#    videos
+#  end
 end
 
 Scraper.new('https://www.youtube.com/results?sp=CAI%253D&q=Boone+NC+bouldering').get_videos_youtube
