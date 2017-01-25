2b4d2cad50fca2b2d4a0fd484464064044ef28af
Liah Wallace <liahwallace1@gmail.com>
2017-01-25 02:01:01 +0000

    rework video to use more instance variables

--- lib/se_climbing_videos/scraper.rb
+++ lib/se_climbing_videos/scraper.rb
@@ -1,30 +1,40 @@
 class SeClimbingVideos::Scraper
 
-  def self.scrape_youtube_list(youtube_search_url)
-    doc = Nokogiri::HTML(open(youtube_search_url))
+  attr_accessor :list_doc
 
-    youtube_videos = []
+  def make_videos(link)
+    @list_doc = Nokogiri::HTML(open(link))
 
     doc.css("div.yt-lockup-content").each do |video|
-      youtube_videos << {
-      :origin = "Youtube",
-      :name = doc.search("a.yt-uix-tile-link").attr("title").text,
-      :upload_user = doc.search("div.yt-lockup-byline").text,
-      :duration = doc.search("span.accessible-description").text.gsub(" - Duration: ", "").gsub(".", ""),
-      :video_url = "https://www.youtube.com" +  doc.search("a.yt-uix-tile-link").attr("href").value}
+      SeClimbingVideos::Video.new_from_youtube_list(video)
     end
-    youtube_videos
   end
 
-  def self.scrape_youtube_video(video_url)
-    video_doc = Nokogiri::HTML(open(video_url))
-
-    video_info = {
-      :description = video_doc.search("#eow-description").text,
-      :upload_date = video_doc.search("#watch-uploader-info").text.gsub("Published on ", "").strip
-      }
-
-    video_info
-  end
+#  def self.scrape_youtube_list(youtube_search_url)
+#    doc = Nokogiri::HTML(open(youtube_search_url))
+#
+#    youtube_videos = []
+#
+#    doc.css("div.yt-lockup-content").each do |video|
+#      youtube_videos << {
+#      :origin = "Youtube",
+#      :name = doc.search("a.yt-uix-tile-link").attr("title").text,
+#      :upload_user = doc.search("div.yt-lockup-byline").text,
+#      :duration = doc.search("span.accessible-description").text.gsub(" - Duration: ", "").gsub(".", ""),
+#      :video_url = "https://www.youtube.com" +  doc.search("a.yt-uix-tile-link").attr("href").value}
+#    end
+#    youtube_videos
+#  end
+#
+#  def self.scrape_youtube_video(video_url)
+#    video_doc = Nokogiri::HTML(open(video_url))
+#
+#    video_info = {
+#      :description = video_doc.search("#eow-description").text,
+#      :upload_date = video_doc.search("#watch-uploader-info").text.gsub("Published on ", "").strip
+#      }
+#
+#    video_info
+#  end
 
 end
--- lib/se_climbing_videos/video.rb
+++ lib/se_climbing_videos/video.rb
@@ -1,16 +1,47 @@
 class SeClimbingVideos::Video
 
-  attr_accessor :name, :location, :upload_date, :upload_user, :description, :duration, :video_url, :origin
+  attr_accessor :name, :location, :upload_date, :upload_user, :description, :duration, :video_url
 
   @@all = []
 
-  def initialize(video_hash)
-    video_hash.each{|key, value| self.send(("#{key}="), value)}
-    @@all << self
+  def self.new_from_youtube_list(video)
+    self.new(
+      SeClimbingVideos::Scraper.list_doc.search("a.yt-uix-tile-link").attr("title").text, SeClimbingVideos::Scraper.list_doc.search("div.yt-lockup-byline").text, SeClimbingVideos::Scraper.list_doc.search("span.accessible-description").text.gsub(" - Duration: ", "").gsub(".", ""), ("https://www.youtube.com" +  SeClimbingVideos::Scraper.list_doc.search("a.yt-uix-tile-link").attr("href").value)
+    )
   end
+
+  def initialize(name=nil, upload_user=nil, duration=nil, video_url=nil)
+    @name = name
+    @upload_user = upload_user
+    @duration = duration
+    @video_url = video_url
+    @location = SEARCH_LINKS[SeClimbingVideos::CLI.location_input][:location]
+    @all << self unless @@all.include?(self)
+  end
+
+#  def initialize(video_hash)
+#    video_hash.each{|key, value| self.send(("#{key}="), value)}
+#    @@all << self
+#  end
 
   def self.all
     @@all
+  end
+
+  def self.find(input)
+    self.all[input-1]
+  end
+
+  def video_doc
+    @video_doc ||= Nokogiri::HTML(open(self.video_url))
+  end
+
+  def description
+    @description ||= @video_doc.search("#eow-description").text
+  end
+
+  def upload_date
+    @upload_date ||= video_doc.search("#watch-uploader-info").text.gsub("Published on ", "").strip
   end
 
 #  def self.all_at_location_by_date(location)
